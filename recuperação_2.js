ac2b4bb16b24c2128759a9d0c6b5e7380a9db2e9
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-09-11 20:02:33 -0300

    dsd

---
+++ recuperação_2.js
@@ -0,0 +1,49 @@
+// Faça um código que declare uma variável nome com seu nome e uma variável cidade com o nome da sua cidade. Exiba ambas as variáveis no terminal.
+
+const input = require('prompt-sync')();
+
+// let pen = input("qual o seu nome:  ")
+// let city = input("qual sua cidade:  ")
+
+// console.log(`o seu nome é ${pen} e a sua cidade ${city}.`)
+
+// Faça um código que declare uma variável num1 com o valor 10 e num2 com o valor 5. Exiba a soma dessas duas variáveis.
+
+// let num1 = 10 
+// let num2 = 5
+
+// console.log(`${num1 + num2}`)
+
+// Faça um código que use prompt-sync para solicitar dois números ao usuário, armazene-os em variáveis e exiba a soma, subtração, multiplicação e divisão.
+
+// let num1 = input( "mande um numero: ")
+// let num2 = input( "mande um outro numero: ")
+
+// console.log(`a soma dos dois numeros  ${num1 + num2} , subtração ${num1 - num2} , a divisao ${ num1 / num2}  e a multiplicaçao ${ num1 * num2} `)
+
+// Faça um código que declare uma variável salario e uma variável aumento (em porcentagem). Calcule e exiba o novo salário após o aumento.
+
+// let wage = 1550.18;
+// let up = 10 ;
+
+// let newwage = wage + ( up * ( newwage / 100));
+// let bank = newwage;
+
+// console.log(`o seu salario nove é ${bank}`)
+
+// Faça um código que peça o nome do aluno e 3 notas, calcule a média e exiba uma mensagem: "A média final de [nome do aluno] é [média].".
+
+
+// const grade = [5 , 3 , 8]
+// let ad = 0
+// let pen = input("qual o nome do aluno;  ")
+
+// for(let i = 0; i < grade.length; i++){
+//    ad += grade[is]
+
+//    let media = ad / grade.length
+
+// }   
+
+// console.log(`o ${pen} possui uma media de ${media}`)
+
---
+++ recupuracao_1.js
@@ -0,0 +1,28 @@
+// Faça um código que solicite o nome do usuário e, em seguida, exiba a mensagem: "Olá, [nome do usuário]!
+
+const input = require('prompt-sync')(); 
+
+// let user = input("qual o seu nome:  ");
+
+// console.log(`olá ${user}`);
+
+// Faça um código que peça a idade do usuário e exiba a mensagem: "Sua idade é [idade do usuário] anos 
+
+// let bday = input("qual a sua idade: ")
+
+// console.log(`a sua idade é ${bday}`)
+
+// Faça um código que solicite o nome e a idade do usuário e exiba uma única mensagem: "Olá, [nome]! Você tem [idade] anos.".
+
+// let age = input("escreva o sua idade :")
+// let pen = input("qual é o seu nome:  ") 
+
+// console.log(`ola ,${pen} ! voce tem ${age} anos`)
+
+// Faça um código que solicite ao usuário o ano de nascimento e o ano atual. Calcule e exiba a idade do usuário
+
+// let year = input("em que ano voce esta agora:  ")
+// let byear = input("qual e o seu ano de nascimento:  ")
+
+// console.log( `o sua idade é ${ year - byear}`)
+
